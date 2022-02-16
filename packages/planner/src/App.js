949e21f79b5188e2613528e518f38b886e78168e
Matthew Murray <mattm898865@gmail.com>
2022-02-15 20:59:15 -0600

    first commit

---
+++ packages/planner/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css'
+import Input from './components/Input'
+import TodoItem from './components/TodoItem';
+import { selectTodoList } from './features/todoSlice';
+import { useSelector } from 'react-redux'
+
+
+function App() {
+
+  const todoList = useSelector(selectTodoList)
+
+  return (
+    <div className="app">
+      <div className='app_container'>
+        <div className='app_todoContainer'>
+          {
+            todoList.map(item => (
+              <TodoItem
+                name={item.item}
+                done={item.done}
+                id={item.id} />
+            ))
+          }
+        </div>
+        <Input />
+      </div>
+    </div>
+  );
+}
+
+export default App;
